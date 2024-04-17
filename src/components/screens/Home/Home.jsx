a4a1c3064f69fcad49f67734c885b6930ce7c7ee
Konstantin <k.kalnizki13@gmail.com>
2024-04-17 17:40:48 +0600

    first commit

---
+++ src/components/screens/Home/Home.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const todos = [
+    {
+        title: 'Read React ',
+        isCompleted: false,
+    },
+    {
+        title: 'Finish the essay collaboration',
+        isCompleted: false,
+    },
+    {
+        title: 'Finish the essay collaboration',
+        isCompleted: false,
+    }
+]
+
+
+const Home = () => {
+    return (
+        <div className="bg-gray-900 h-screen text-white">
+
+
+
+        </div>
+    )
+}
+
+export default Home
